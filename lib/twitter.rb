1e7518591fc937155aecb6b669d6786cef45b763
Chris Shea <chris@tie-rack.org>
2009-01-31 12:57:32 -0700

    Split out search from user stuff

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -2,13 +2,33 @@ require 'rubygems'
 require 'httparty'
 
 module Twitter
-  include HTTParty
-  base_uri('search.twitter.com')
-  format(:json)
 
-  class << self
-    def find_by_user_and_tag(user, tag)
-      get("/search.json?q=from:#{user}+%23#{tag}")
+  module Search
+    include HTTParty
+    format(:json)
+    base_uri('search.twitter.com')
+
+    class << self
+      def find_by_user_and_tag(user, tag)
+        get("/search.json?q=from:#{user}+%23#{tag}")
+      end
+
+      def find_by_tag(tag)
+        get("/search.json?q=%23#{tag}")
+      end
     end
   end
+
+  module User
+    include HTTParty
+    format(:json)
+    base_uri('twitter.com')
+
+    class << self
+      def timeline(user)
+        get("/statuses/user_timeline/#{user}.json")
+      end
+    end
+  end
+
 end
