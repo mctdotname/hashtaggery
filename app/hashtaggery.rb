99e7c71220942ed919b9b497183447aace514d42
Chris Shea <chris@tie-rack.org>
2009-01-31 12:57:46 -0700

    /tag and /user routes

--- app/hashtaggery.rb
+++ app/hashtaggery.rb
@@ -6,6 +6,17 @@ $: << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'twitter'
 
 get '/user/:user/tag/:tag' do
-  tweets = Twitter.find_by_user_and_tag(params[:user], params[:tag])
+  tweets = Twitter::Search.find_by_user_and_tag(params[:user], params[:tag])
   tweets['results'].map {|tweet| tweet['text']}.join('<br />')
+end
+
+get '/tag/:tag' do
+  tweets = Twitter::Search.find_by_tag(params[:tag])
+  tweets['results'].map {|tweet| tweet['from_user'] + ': ' + tweet['text']}.join('<br />')
+end
+
+get '/user/:user' do
+  tweets = Twitter::User.timeline(params[:user])
+#   tweets['results'].map {|tweet| tweet['text']}.join('<br />')
+  tweets.inspect
 end
